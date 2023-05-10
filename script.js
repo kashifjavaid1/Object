f69ea0ba04676398a25213949b9f316e4943c1e0
kashifjavaid1 <gmdcf797@gmail.com>
2023-05-10 21:50:49 +0500

    javasctipt file
    create fist object students_name variable name

---
+++ script.js
@@ -0,0 +1,46 @@
+ var student_data=[{
+    Name:"ali",
+    roll_No:612705,
+    Department:"BSIT",
+    semester:"4th",
+    age:17,
+    skill:"web desgin",
+ }, 
+//  {
+//     Name:"abubaker",
+//     roll_No:612708,
+//     Department:"BSIT",
+//     semester:"4th",
+//     age:18,
+//     skill:"video editing",
+//  },
+//  {
+//     Name:"kashif",
+//     roll_No:612709,
+//     Department:"BSIT",
+//     semester:"4th",
+//     age:21,
+//     skill:"Mobile Apps"
+//  },
+// {
+//     Name:"Waleed",
+//     roll_No:612710,
+//     Department:"BSIT",
+//     semester:"4th",
+//     age:22,
+//     skill:"Graphic design",
+// },
+// {
+//     Name:"umair",
+//     roll_No:612711,
+//     Department:"BSIT",
+//     semester:"4th",
+//     age:19,
+//     skill:"c++",
+// }]
+// for (var abc= 0; abc< student_data.length; abc++) {
+//    const elemt=student_data[abc];
+//    if (elemt.Name=="kashif") {
+//     console.log(elemt.skill);
+//    }
+// }
